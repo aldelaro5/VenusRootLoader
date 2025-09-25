fb24690d69d47c18241289ee3daa2cc07f204c64
aldelaro5 <aldelaro5@gmail.com>
2025-09-24 23:43:31 -0400

    Make the CsWin32 types public
    Also removes runtime marshalling since it's apparently not recommended in native aot

--- VenusRootLoader.Bootstrap/Shared/IWin32.cs
+++ VenusRootLoader.Bootstrap/Shared/IWin32.cs
@@ -12,21 +12,21 @@ namespace VenusRootLoader.Bootstrap.Shared;
 
 public interface IWin32
 {
-    internal FreeLibrarySafeHandle GetModuleHandle(string lpModuleName);
-    internal FARPROC GetProcAddress(HMODULE hModule, PCSTR lpProcName);
-    internal HANDLE GetStdHandle(STD_HANDLE nStdHandle);
-    internal HANDLE CreateFile(PCWSTR lpFileName, uint dwDesiredAccess, FILE_SHARE_MODE dwShareMode, Pointer<SECURITY_ATTRIBUTES> lpSecurityAttributes, FILE_CREATION_DISPOSITION dwCreationDisposition, FILE_FLAGS_AND_ATTRIBUTES dwFlagsAndAttributes, HANDLE hTemplateFile);
-    internal BOOL ReadFile(HANDLE hFile, Pointer<byte> lpBuffer, uint nNumberOfBytesToRead, Pointer<uint> lpNumberOfBytesRead, Pointer<NativeOverlapped> lpOverlapped);
-    internal BOOL WriteFile(HANDLE hFile, Pointer<byte> lpBuffer, uint nNumberOfBytesToWrite, Pointer<uint> lpNumberOfBytesWritten, Pointer<NativeOverlapped> lpOverlapped);
-    internal BOOL CloseHandle(HANDLE hObject);
-    internal MESSAGEBOX_RESULT MessageBox(HWND hWnd, string lpText, string lpCaption, MESSAGEBOX_STYLE uType);
-    internal BOOL ShowWindow(HWND hWnd, SHOW_WINDOW_CMD nCmdShow);
-    internal HWND GetConsoleWindow();
-    internal BOOL SetFilePointerEx(HANDLE hFile, long liDistanceToMove, Pointer<long> lpNewFilePointer, SET_FILE_POINTER_MOVE_METHOD dwMoveMethod);
-    internal int sendto(SOCKET s, PCSTR buf, int len, int flags, Pointer<SOCKADDR> to, int toLen);
-    internal uint GetModuleFileName(HMODULE hModule, PWSTR lpFilename, uint nSize);
-    internal int send(SOCKET s, PCSTR buf, int len, SEND_RECV_FLAGS flags);
-    internal int recv(SOCKET s, PSTR buf, int len, SEND_RECV_FLAGS flags);
-    internal BOOL GetConsoleMode(HANDLE hConsoleHandle, Pointer<CONSOLE_MODE> lpMode);
-    internal BOOL SetConsoleMode(HANDLE hConsoleHandle, CONSOLE_MODE dwMode);
+    public FreeLibrarySafeHandle GetModuleHandle(string lpModuleName);
+    public FARPROC GetProcAddress(HMODULE hModule, PCSTR lpProcName);
+    public HANDLE GetStdHandle(STD_HANDLE nStdHandle);
+    public HANDLE CreateFile(PCWSTR lpFileName, uint dwDesiredAccess, FILE_SHARE_MODE dwShareMode, Pointer<SECURITY_ATTRIBUTES> lpSecurityAttributes, FILE_CREATION_DISPOSITION dwCreationDisposition, FILE_FLAGS_AND_ATTRIBUTES dwFlagsAndAttributes, HANDLE hTemplateFile);
+    public BOOL ReadFile(HANDLE hFile, Pointer<byte> lpBuffer, uint nNumberOfBytesToRead, Pointer<uint> lpNumberOfBytesRead, Pointer<NativeOverlapped> lpOverlapped);
+    public BOOL WriteFile(HANDLE hFile, Pointer<byte> lpBuffer, uint nNumberOfBytesToWrite, Pointer<uint> lpNumberOfBytesWritten, Pointer<NativeOverlapped> lpOverlapped);
+    public BOOL CloseHandle(HANDLE hObject);
+    public MESSAGEBOX_RESULT MessageBox(HWND hWnd, string lpText, string lpCaption, MESSAGEBOX_STYLE uType);
+    public BOOL ShowWindow(HWND hWnd, SHOW_WINDOW_CMD nCmdShow);
+    public HWND GetConsoleWindow();
+    public BOOL SetFilePointerEx(HANDLE hFile, long liDistanceToMove, Pointer<long> lpNewFilePointer, SET_FILE_POINTER_MOVE_METHOD dwMoveMethod);
+    public int sendto(SOCKET s, PCSTR buf, int len, int flags, Pointer<SOCKADDR> to, int toLen);
+    public uint GetModuleFileName(HMODULE hModule, PWSTR lpFilename, uint nSize);
+    public int send(SOCKET s, PCSTR buf, int len, SEND_RECV_FLAGS flags);
+    public int recv(SOCKET s, PSTR buf, int len, SEND_RECV_FLAGS flags);
+    public BOOL GetConsoleMode(HANDLE hConsoleHandle, Pointer<CONSOLE_MODE> lpMode);
+    public BOOL SetConsoleMode(HANDLE hConsoleHandle, CONSOLE_MODE dwMode);
 }

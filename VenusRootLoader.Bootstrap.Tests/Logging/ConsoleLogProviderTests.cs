e1a969706e8e0e17cee6012dafa2fbc5c7d63eb1
aldelaro5 <aldelaro5@gmail.com>
2025-09-25 22:17:06 -0400

    Add ConsoleLogProvider tests

--- VenusRootLoader.Bootstrap.Tests/Logging/ConsoleLogProviderTests.cs
+++ VenusRootLoader.Bootstrap.Tests/Logging/ConsoleLogProviderTests.cs
@@ -1,5 +1,166 @@
+using System.Runtime.CompilerServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Console;
+using AwesomeAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using VenusRootLoader.Bootstrap.Logging;
+using VenusRootLoader.Bootstrap.Settings.LogProvider;
+using VenusRootLoader.Bootstrap.Shared;
+
 namespace VenusRootLoader.Bootstrap.Tests.Logging;
 
 public sealed class ConsoleLogProviderTests
 {
+    private readonly IOptions<ConsoleLoggerSettings> _consoleLoggerOptions = Substitute.For<IOptions<ConsoleLoggerSettings>>();
+    private readonly TimeProvider _timeProvider = Substitute.For<TimeProvider>();
+    private readonly IWin32 _win32 = Substitute.For<IWin32>();
+    
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_renderingMode")]
+    private static extern ref ConsoleLogProvider.RenderingMode ConsoleLogProviderRenderingMode(ConsoleLogger provider);
+
+
+    private ConsoleLogProvider _sut = null!;
+
+    [Fact]
+    public void CreateLogger_ReturnsNullLogger_WhenConsoleLoggerIsDisabled()
+    {
+        _consoleLoggerOptions.Value.Returns(new ConsoleLoggerSettings
+        {
+            Enable = false,
+            LogWithColors = true
+        });
+        var gameExecutionContext = new GameExecutionContext
+        {
+            LibraryHandle = 0,
+            GameDir = "",
+            DataDir = "",
+            UnityPlayerDllFileName = "UnityPlayer.dll",
+            IsWine = false
+        };
+
+        _sut = new(gameExecutionContext, _consoleLoggerOptions, _win32, _timeProvider);
+        var logger = _sut.CreateLogger("category");
+
+        logger.Should().BeOfType<NullLogger>();
+    }
+
+    [Fact]
+    public void CreateLogger_ReturnsConsoleLoggerWithNoColors_WhenColorsAreDisabled()
+    {
+        _consoleLoggerOptions.Value.Returns(new ConsoleLoggerSettings
+        {
+            Enable = true,
+            LogWithColors = false
+        });
+        var gameExecutionContext = new GameExecutionContext
+        {
+            LibraryHandle = 0,
+            GameDir = "",
+            DataDir = "",
+            UnityPlayerDllFileName = "UnityPlayer.dll",
+            IsWine = false
+        };
+
+        _sut = new(gameExecutionContext, _consoleLoggerOptions, _win32, _timeProvider);
+        var logger = _sut.CreateLogger("category");
+
+        logger.Should().BeOfType<ConsoleLogger>();
+        ConsoleLogProviderRenderingMode((ConsoleLogger)logger)
+            .Should().Be(ConsoleLogProvider.RenderingMode.NoColors);
+    }
+
+    [Theory]
+    [InlineData(STD_HANDLE.STD_OUTPUT_HANDLE)]
+    [InlineData(STD_HANDLE.STD_ERROR_HANDLE)]
+    public void CreateLogger_ReturnsConsoleLoggerWithLegacyColors_WhenColorsAreEnabledAndAnsiIsNotSupported(STD_HANDLE stdHandleWithoutAnsi)
+    {
+        var handleWithoutAnsi = (HANDLE)Random.Shared.Next();
+        _consoleLoggerOptions.Value.Returns(new ConsoleLoggerSettings
+        {
+            Enable = true,
+            LogWithColors = true
+        });
+        var gameExecutionContext = new GameExecutionContext
+        {
+            LibraryHandle = 0,
+            GameDir = "",
+            DataDir = "",
+            UnityPlayerDllFileName = "UnityPlayer.dll",
+            IsWine = false
+        };
+
+        _win32.GetStdHandle(stdHandleWithoutAnsi).Returns(handleWithoutAnsi);
+        _win32.SetConsoleMode(handleWithoutAnsi, Arg.Any<CONSOLE_MODE>()).Returns((BOOL)false);
+
+        _sut = new(gameExecutionContext, _consoleLoggerOptions, _win32, _timeProvider);
+        var logger = _sut.CreateLogger("category");
+
+        logger.Should().BeOfType<ConsoleLogger>();
+        ConsoleLogProviderRenderingMode((ConsoleLogger)logger)
+            .Should().Be(ConsoleLogProvider.RenderingMode.LegacyColors);
+    }
+
+    [Fact]
+    public void CreateLogger_ReturnsConsoleLoggerWithLegacyColors_WhenUsingWine()
+    {
+        var stdOutHandle = (HANDLE)Random.Shared.Next();
+        var stdErrHandle = (HANDLE)Random.Shared.Next();
+        _consoleLoggerOptions.Value.Returns(new ConsoleLoggerSettings
+        {
+            Enable = true,
+            LogWithColors = true
+        });
+        var gameExecutionContext = new GameExecutionContext
+        {
+            LibraryHandle = 0,
+            GameDir = "",
+            DataDir = "",
+            UnityPlayerDllFileName = "UnityPlayer.dll",
+            IsWine = true
+        };
+        _win32.GetStdHandle(STD_HANDLE.STD_OUTPUT_HANDLE).Returns(stdOutHandle);
+        _win32.GetStdHandle(STD_HANDLE.STD_ERROR_HANDLE).Returns(stdErrHandle);
+        _win32.SetConsoleMode(stdOutHandle, Arg.Any<CONSOLE_MODE>()).Returns((BOOL)true);
+        _win32.SetConsoleMode(stdErrHandle, Arg.Any<CONSOLE_MODE>()).Returns((BOOL)true);
+
+        _sut = new(gameExecutionContext, _consoleLoggerOptions, _win32, _timeProvider);
+        var logger = _sut.CreateLogger("category");
+
+        logger.Should().BeOfType<ConsoleLogger>();
+        ConsoleLogProviderRenderingMode((ConsoleLogger)logger)
+            .Should().Be(ConsoleLogProvider.RenderingMode.LegacyColors);
+    }
+
+    [Fact]
+    public void CreateLogger_ReturnsConsoleLoggerWithAnsiColors_WhenSupportedAndNotUsingWine()
+    {
+        var stdOutHandle = (HANDLE)Random.Shared.Next();
+        var stdErrHandle = (HANDLE)Random.Shared.Next();
+        _consoleLoggerOptions.Value.Returns(new ConsoleLoggerSettings
+        {
+            Enable = true,
+            LogWithColors = true
+        });
+        var gameExecutionContext = new GameExecutionContext
+        {
+            LibraryHandle = 0,
+            GameDir = "",
+            DataDir = "",
+            UnityPlayerDllFileName = "UnityPlayer.dll",
+            IsWine = false
+        };
+        _win32.GetStdHandle(STD_HANDLE.STD_OUTPUT_HANDLE).Returns(stdOutHandle);
+        _win32.GetStdHandle(STD_HANDLE.STD_ERROR_HANDLE).Returns(stdErrHandle);
+        _win32.SetConsoleMode(stdOutHandle, Arg.Any<CONSOLE_MODE>()).Returns((BOOL)true);
+        _win32.SetConsoleMode(stdErrHandle, Arg.Any<CONSOLE_MODE>()).Returns((BOOL)true);
+
+        _sut = new(gameExecutionContext, _consoleLoggerOptions, _win32, _timeProvider);
+        var logger = _sut.CreateLogger("category");
+
+        logger.Should().BeOfType<ConsoleLogger>();
+        ConsoleLogProviderRenderingMode((ConsoleLogger)logger)
+            .Should().Be(ConsoleLogProvider.RenderingMode.AnsiColors);
+    }
 }

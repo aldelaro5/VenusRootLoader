9533a2c6a49132e031c04c83042eec5fe7028e74
aldelaro5 <aldelaro5@gmail.com>
2025-09-26 23:29:23 -0400

    Add custom env var config provider and source tests

--- VenusRootLoader.Bootstrap.Tests/Settings/EnvironmentConfiguration/CustomEnvironmentVariablesConfigurationProviderTests.cs
+++ VenusRootLoader.Bootstrap.Tests/Settings/EnvironmentConfiguration/CustomEnvironmentVariablesConfigurationProviderTests.cs
@@ -1,6 +1,32 @@
+using System.Runtime.CompilerServices;
+using AwesomeAssertions;
+using Microsoft.Extensions.Configuration;
+using VenusRootLoader.Bootstrap.Settings.EnvironmentConfiguration;
+
 namespace VenusRootLoader.Bootstrap.Tests.Settings.EnvironmentConfiguration;
 
 public class CustomEnvironmentVariablesConfigurationProviderTests
 {
-    
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_Data")]
+    private static extern IDictionary<string, string?> CustomEnvVarConfigProviderData(ConfigurationProvider provider);
+
+    [Fact]
+    public void Load_SetCorrectConfigData_WhenCalled()
+    {
+        var prefix = "PREFIX_";
+        var configKey = "ConfigKey";
+        var value = "true";
+        var mappings = new Dictionary<string, string>
+        {
+            ["EXIST"] = configKey
+        };
+        var sut = new CustomEnvironmentVariablesConfigurationProvider(prefix, mappings);
+        Environment.SetEnvironmentVariable("NO_PREFIX", value);
+        Environment.SetEnvironmentVariable($"{prefix}DOES_NOT_EXIST", value);
+        Environment.SetEnvironmentVariable($"{prefix}EXIST", value);
+        sut.Load();
+
+        CustomEnvVarConfigProviderData(sut)
+            .Should().ContainSingle(kvp => kvp.Key == configKey && kvp.Value == value);
+    }
 }
--- VenusRootLoader.Bootstrap.Tests/Settings/EnvironmentConfiguration/CustomEnvironmentVariablesConfigurationSourceTests.cs
+++ VenusRootLoader.Bootstrap.Tests/Settings/EnvironmentConfiguration/CustomEnvironmentVariablesConfigurationSourceTests.cs
@@ -1,6 +1,38 @@
+using System.Runtime.CompilerServices;
+using AwesomeAssertions;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using VenusRootLoader.Bootstrap.Settings.EnvironmentConfiguration;
+
 namespace VenusRootLoader.Bootstrap.Tests.Settings.EnvironmentConfiguration;
 
 public class CustomEnvironmentVariablesConfigurationSourceTests
 {
-    
+    private readonly IConfigurationBuilder _configuration = Substitute.For<IConfigurationBuilder>();
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_prefix")]
+    private static extern ref string CustomEnvVarConfigProviderPrefix(CustomEnvironmentVariablesConfigurationProvider provider);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_environmentVariablesMapping")]
+    private static extern ref IDictionary<string, string> CustomEnvVarConfigProviderMappings(CustomEnvironmentVariablesConfigurationProvider provider);
+
+    [Fact]
+    public void Build_GivesCorrectBuilder_WhenCalled()
+    {
+        var sut = new CustomEnvironmentVariablesConfigurationSource
+        {
+            Prefix = "PREFIX_",
+            EnvironmentVariablesMapping = new Dictionary<string, string>
+            {
+                ["a"] = "b"
+            }
+        };
+        var result = sut.Build(_configuration);
+
+        result.Should().BeOfType<CustomEnvironmentVariablesConfigurationProvider>();
+        CustomEnvVarConfigProviderPrefix((CustomEnvironmentVariablesConfigurationProvider)result)
+            .Should().Be(sut.Prefix);
+        CustomEnvVarConfigProviderMappings((CustomEnvironmentVariablesConfigurationProvider)result)
+            .Should().BeSameAs(sut.EnvironmentVariablesMapping);
+    }
 }

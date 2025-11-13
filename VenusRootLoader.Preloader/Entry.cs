e9765510256725ae34bbcb8b519a48d28a98cf2b
aldelaro5 <aldelaro5@gmail.com>
2025-11-12 19:52:57 -0500

    Make the preloader only depend on MonoMod
    Also added some comments and simplified the whole thing. The need to force load MonoMod.Core is curious, but this works for now without spinning a Harmony instance so it will do.

--- VenusRootLoader.Preloader/Entry.cs
+++ VenusRootLoader.Preloader/Entry.cs
@@ -1,59 +1,69 @@
-using HarmonyLib;
-using Microsoft.Extensions.Logging;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-namespace VenusRootLoader.Preloader;
-
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Global
+
+namespace VenusRootLoader.Preloader;
+
+/// <summary>
+/// The preloader's sole purpose is to be the bridge between the bootstrap and the loader.
+/// This class implements a loader entrypoint by runtime patching a Unity method that is invoked
+/// early, but late enough so that all UnityEngine and game assemblies are available for usage.
+/// It is too early in this assembly to reference the loader assembly, any UnityEngine or game assemblies
+/// so this entry is done via Reflection and an AssemblyLoad AppDomain event. The preloader is designed to leave as
+/// few traces as possible once the loader entrypoint is setup.
+/// </summary>
 internal class Entry
 {
+    /// <summary>
+    /// Copied from Microsoft.Extensions.Logging, it's only there to not have a reference to it at runtime
+    /// </summary>
+    internal enum LogLevel
+    {
+        Trace = 0,
+        Debug = 1,
+        Information = 2,
+        Warning = 3,
+        Error = 4,
+        Critical = 5,
+        None = 6,
+    }
+
     [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
-    internal delegate void LogMsgFn(string message, string category, LogLevel logLevel);
+    internal delegate void BootstrapLogFn(string message, string category, LogLevel logLevel);
 
-    internal static LogMsgFn LogMsg = null!;
+    // We want some minimal logging capabilities to monitor the booting so we use the log function directly.
+    internal static BootstrapLogFn BootstrapLogMsg = null!;
 
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
-    internal delegate void FreeGameExecutionContextFn(nint gameExecutionContextPtr);
-
-    internal static FreeGameExecutionContextFn FreeGameExecutionContext = null!;
-
-    internal static nint RelayLogFunctionPtr;
+    internal static nint BootstrapLogFunctionPtr;
     internal static nint GameExecutionContextPtr;
 
-    private const string LogCategory = "VenusRootLoader.Preloader";
+    private static readonly string LogCategory = typeof(Entry).Namespace!;
+
+    // This particular entrypoint occurs once the earliest after the engine is mostly initialized
     private const string GameLoadHookAssemblyName = "UnityEngine.CoreModule";
     private const string GameLoadHookTypeName = "UnityEngine.SceneManagement.SceneManager";
     private const string GameLoadHookMethodName = "Internal_ActiveSceneChanged";
 
     private static bool _monoCoreStartEntrypointAlreadyCalled;
 
-    private static readonly Harmony HarmonyInstance = new(LogCategory);
+    private static ILHook _loaderEntrypointHook = null!;
 
-    private static readonly string[] CriticalAssemblies =
-    [
-        "Mono.Cecil.dll",
-        "Mono.Cecil.Mdb.dll",
-        "Mono.Cecil.Pdb.dll",
-        "Mono.Cecil.Rocks.dll",
-        "MonoMod.Core.dll",
-        "MonoMod.Iced.dll",
-        "MonoMod.ILHelpers.dll",
-        "MonoMod.Utils.dll",
-        "MonoMod.RuntimeDetour.dll",
-        "MonoMod.Backports.dll",
-        "0Harmony.dll"
-    ];
-
-    internal static void Main(nint relayLogFunctionPtr, nint gameExecutionContextPtr)
+    /// <summary>
+    /// This is the entry method called by the bootstrap. It receives 2 parameters that are meant to be forwarded to the loader.
+    /// </summary>
+    /// <param name="bootstrapLogFunctionPtr">A raw pointer to a <see cref="BootstrapLogFn"/> function for logging</param>
+    /// <param name="gameExecutionContextPtr">A raw pointer to a struct containing information about the execution</param>
+    internal static void Main(nint bootstrapLogFunctionPtr, nint gameExecutionContextPtr)
     {
         string pathAssemblies = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "VenusRootLoader");
-        foreach (string criticalAssembly in CriticalAssemblies)
-            Assembly.LoadFrom(Path.Combine(pathAssemblies, criticalAssembly));
+        Assembly.LoadFrom(Path.Combine(pathAssemblies, "MonoMod.Core.dll"));
 
-        RelayLogFunctionPtr = relayLogFunctionPtr;
-        LogMsg = Marshal.GetDelegateForFunctionPointer<LogMsgFn>(relayLogFunctionPtr);
+        BootstrapLogFunctionPtr = bootstrapLogFunctionPtr;
+        BootstrapLogMsg = Marshal.GetDelegateForFunctionPointer<BootstrapLogFn>(bootstrapLogFunctionPtr);
         GameExecutionContextPtr = gameExecutionContextPtr;
 
         AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
@@ -62,7 +72,7 @@ internal class Entry
 
     private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
     {
-        LogMsg($"Unhandled exception: {e.ExceptionObject}", LogCategory, LogLevel.Error);
+        BootstrapLogMsg($"Unhandled exception: {e.ExceptionObject}", LogCategory, LogLevel.Error);
     }
 
     // This hook allows to have a suitable GameLoad entrypoint without actually referencing the Unity assemblies
@@ -77,30 +87,43 @@ internal class Entry
         {
             AppDomain.CurrentDomain.AssemblyLoad -= OnAssemblyLoad;
 
-            var sceneManagerType = assembly.GetType(GameLoadHookTypeName, false);
-            var original = AccessTools.Method(sceneManagerType, GameLoadHookMethodName);
-            var harmonyMethod = new HarmonyMethod(typeof(Entry), nameof(Entrypoint));
-            HarmonyInstance.Patch(original, prefix: harmonyMethod);
-            LogMsg($"Hooked into {original.FullDescription()}", LogCategory, LogLevel.Information);
+            Type sceneManagerType = assembly.GetType(GameLoadHookTypeName, false);
+            MethodInfo original = sceneManagerType.GetMethod(
+                GameLoadHookMethodName,
+                BindingFlags.Static | BindingFlags.NonPublic)!;
+            MethodInfo entryPoint = typeof(Entry).GetMethod(
+                nameof(Entrypoint),
+                BindingFlags.Static | BindingFlags.NonPublic)!;
+
+            _loaderEntrypointHook = new ILHook(
+                original,
+                il =>
+                {
+                    ILCursor cursor = new(il);
+                    cursor.EmitCall(entryPoint);
+                });
+            BootstrapLogMsg($"Hooked into {original.Name}", LogCategory, LogLevel.Information);
         }
         catch (Exception e)
         {
-            LogMsg($"Exception when setting up the GameLoad entry: {e}", LogCategory, LogLevel.Error);
+            BootstrapLogMsg($"Exception when setting up the GameLoad entry: {e}", LogCategory, LogLevel.Error);
         }
     }
 
-    private static bool Entrypoint()
+    // This is the final stage of the loader entrypoint. When this is called for the first time thanks to the ILHook,
+    // the entrypoint is complete and the loader entry can be called via reflection
+    private static void Entrypoint()
     {
         if (_monoCoreStartEntrypointAlreadyCalled)
-            return true;
+            return;
         _monoCoreStartEntrypointAlreadyCalled = true;
+        _loaderEntrypointHook.Dispose();
 
         MethodInfo entryMethod = Assembly.Load("VenusRootLoader")
             .GetType("VenusRootLoader.Entry")
-            .Method("Main");
+            .GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic)!;
         AppDomain.CurrentDomain.UnhandledException -= OnUnhandledException;
 
-        entryMethod.Invoke(null, [RelayLogFunctionPtr, GameExecutionContextPtr]);
-        return true;
+        entryMethod.Invoke(null, [BootstrapLogFunctionPtr, GameExecutionContextPtr]);
     }
 }

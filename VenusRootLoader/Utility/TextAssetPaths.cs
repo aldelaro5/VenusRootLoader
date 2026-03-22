8eafce7fb1fb20d94a048ed904f9231d40875e80
aldelaro5 <aldelaro5@gmail.com>
2026-03-22 18:54:50 -0400

    Add static class with most resources path in it

---
+++ VenusRootLoader/Utility/TextAssetPaths.cs
@@ -0,0 +1,91 @@
+namespace VenusRootLoader.Utility;
+
+public static class TextAssetPaths
+{
+    internal const string AnimatorControllerdPathPrefix = "AnimationControllers/";
+    internal const string AudioPathPrefix = "Audio/";
+    internal const string DataPathPrefix = "Data/";
+    internal const string FontdPathPrefix = "Fonts/";
+    internal const string MaterialsPathPrefix = "Materials/";
+    internal const string PrefabsPathPrefix = "Prefabs/";
+    internal const string ShadersPathPrefix = "Shaders/";
+    internal const string SpritesPathPrefix = "Sprites/";
+
+    internal const string MusicDirectory = $"{AudioPathPrefix}Music/";
+    internal const string SoundsDirectory = $"{AudioPathPrefix}Sounds/";
+    internal const string DialogueSoundsDirectory = $"{SoundsDirectory}Dialogue/";
+
+    internal const string LocalizedDialoguesPathPrefix = "Data/Dialogues";
+    internal const string MapEntitiesDataDirectory = $"{DataPathPrefix}EntityData/";
+    internal const string FishingItemsPath = $"{DataPathPrefix}FishingItems";
+    internal const string FishingFishesDirectory = $"{DataPathPrefix}Fishing/Fish/";
+    internal const string MedalsDataPath = $"{DataPathPrefix}BadgeData";
+    internal const string MedalsOrderingPath = $"{DataPathPrefix}BadgeOrder";
+    internal const string QuestsDataPath = $"{DataPathPrefix}BoardData";
+    internal const string SpyCardsDataPath = $"{DataPathPrefix}CardData";
+    internal const string SpyCardsOrderingPath = $"{DataPathPrefix}CardOrder";
+    internal const string RecipesLibraryEntriesInputItemsPath = $"{DataPathPrefix}CookLibrary";
+    internal const string RecipesLibraryEntriesResultItemsPath = $"{DataPathPrefix}CookOrder";
+    internal const string DiscoveriesOrderingPath = $"{DataPathPrefix}DiscoveryOrder";
+    internal const string EnemiesDataPath = $"{DataPathPrefix}EnemyData";
+    internal const string AnimIdsDataPath = $"{DataPathPrefix}EntityValues";
+    internal const string ItemsDataPath = $"{DataPathPrefix}ItemData";
+    internal const string LanguageHelpTextsPath = $"{DataPathPrefix}LanguageHelp";
+    internal const string BattleLeafPositionsInTransitionPath = $"{DataPathPrefix}LeafPos";
+    internal const string LetterPromptDataPathPrefix = $"{DataPathPrefix}LetterPrompt";
+    internal const string RankBonusesDataPath = $"{DataPathPrefix}LevelData";
+    internal const string MusicLoopPointsDataPath = $"{DataPathPrefix}LoopPoints";
+    internal const string QuestsRequirementsDataPath = $"{DataPathPrefix}QuestChecks";
+    internal const string RecipesDataPath = $"{DataPathPrefix}RecipeData";
+    internal const string SkillsDataPath = $"{DataPathPrefix}SkillData";
+    internal const string RecordsOrderingPath = $"{DataPathPrefix}SynopsisOrder";
+    internal const string BestiaryEntriesOrderingPath = $"{DataPathPrefix}TattleList";
+    internal const string TermacadePrizesDataPath = $"{DataPathPrefix}Termacade";
+    internal const string TestRoomMapDialoguesPath = $"{DataPathPrefix}TestRoom";
+    internal const string CaveOfTrialsBattlesDataPath = $"{DataPathPrefix}Trials";
+
+    internal const string LocalizedMapDialoguesDirectorySubPath = "/Maps";
+    internal const string LocalizedActionCommandHelpTextsPathSuffix = "/ActionCommands";
+    internal const string LocalizedAreaDescriptionsPathSuffix = "/AreaDesc";
+    internal const string LocalizedAreaNamesPathSuffix = "/AreaNames";
+    internal const string LocalizedMedalDataPathSuffix = "/BadgeName";
+    internal const string LocalizedQuestsDataPathSuffix = "/BoardQuests";
+    internal const string LocalizedSpyCardsTextsPathSuffix = "/CardDialogue";
+    internal const string LocalizedSpyCardsDataPathSuffix = "/CardText";
+    internal const string LocalizedCommonDialoguesPathSuffix = "/CommonDialogue";
+    internal const string LocalizedCreditsPathSuffix = "/Credits";
+    internal const string LocalizedDiscoveriesDataPathSuffix = "/Discoveries";
+    internal const string LocalizedBestiaryEntriesDataPathSuffix = "/EnemyTattle";
+    internal const string LocalizedFishingTextsPathSuffix = "/Fishing";
+    internal const string LocalizedCrystalBerryFortuneTellerHintsPathSuffix = "/FortuneTeller0";
+    internal const string LocalizedLoreBookFortuneTellerHintsPathSuffix = "/FortuneTeller1";
+    internal const string LocalizedMedalFortuneTellerHintsPathSuffix = "/FortuneTeller2";
+    internal const string LocalizedItemsDataPathSuffix = "/Items";
+    internal const string LocalizedLoreBooksDataPathSuffix = "/LoreText";
+    internal const string LocalizedMenuTextsPathSuffix = "/MenuText";
+    internal const string LocalizedMusicNamesPathSuffix = "/MusicList";
+    internal const string LocalizedSkillsDataPathSuffix = "/Skills";
+    internal const string LocalizedRecordsDataPathSuffix = "/Synopsis";
+
+    internal const string BattleMapsPrefabsDirectory = $"{PrefabsPathPrefix}BattleMaps";
+    internal const string DisguisesPrefabsDirectory = $"{PrefabsPathPrefix}Disguises";
+    internal const string MapsPrefabsDirectory = $"{PrefabsPathPrefix}Maps";
+    internal const string ObjectPrefabsDirectory = $"{PrefabsPathPrefix}Objects";
+    internal const string ParticlesPrefabsDirectory = $"{PrefabsPathPrefix}Particles";
+
+    internal const string EntitySpritesDirectory = $"{SpritesPathPrefix}Entities";
+    internal const string GuiSpritesDirectory = $"{SpritesPathPrefix}Gui";
+    internal const string EnemyPortraitSpritesPath = $"{SpritesPathPrefix}Items/EnemyPortraits";
+    internal const string Items0SpritesPath = $"{SpritesPathPrefix}Items/Items0";
+    internal const string Items1SpritesPath = $"{SpritesPathPrefix}Items/Items1";
+
+    internal const string Gui9BoxSpritesDirectory = $"{SpritesPathPrefix}Gui/9Box";
+    internal const string LocalizesBattleMessageSpritesPathPrefix = $"{SpritesPathPrefix}Gui/BattleMessage/Battlem";
+    internal const string LocalizesRankUpSpritePathPrefix = $"{SpritesPathPrefix}Gui/BattleMessage/Rank";
+    internal const string LeafInBattleTransitionSpritesPath = $"{SpritesPathPrefix}Gui/BattleLeaves";
+    internal const string EmoticonSpritesPath = $"{SpritesPathPrefix}Gui/Emoticons";
+    internal const string Gui1SpritesPath = $"{SpritesPathPrefix}Gui/Gui";
+    internal const string Gui2SpritesPath = $"{SpritesPathPrefix}Gui/Gui2";
+    internal const string TextBoxSpritesPath = $"{SpritesPathPrefix}Gui/Textbox";
+    internal const string LocalizesGameTitleSpritePathPrefix = $"{SpritesPathPrefix}Gui/Title";
+}

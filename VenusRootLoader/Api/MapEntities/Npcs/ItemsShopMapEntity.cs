1af5b8ca867b5c862b3d9bda9aba2cfa0742183c
aldelaro5 <aldelaro5@gmail.com>
2026-05-23 11:17:56 -0400

    Initial Items Shop API

--- VenusRootLoader/Api/MapEntities/Npcs/ItemsShopMapEntity.cs
+++ VenusRootLoader/Api/MapEntities/Npcs/ItemsShopMapEntity.cs
@@ -1,9 +1,12 @@
+using System.Collections.ObjectModel;
 using UnityEngine;
 using VenusRootLoader.Api.Leaves;
 using VenusRootLoader.Api.MapEntities.ActionBehaviors;
 using VenusRootLoader.Registry;
 
 namespace VenusRootLoader.Api.MapEntities.Npcs;
+
+using ShelvedItemForSale = (Branch<ItemLeaf> Item, Vector3 Position);
 
 public sealed class ItemsShopMapEntity : MapEntity
 {
@@ -22,6 +25,33 @@ public sealed class ItemsShopMapEntity : MapEntity
             field = value;
         }
     }
+
+    public int DialogueIdWhenInteractingWithShopKeeper
+    {
+        get => (int)InternalDialogues[0].y;
+        set => InternalDialogues[0] = new(InternalDialogues[0].x, value, InternalDialogues[0].z);
+    }
+
+    public float? ItemsBuyingPriceMultiplier
+    {
+        get => InternalDialogues[2].y > 0.1f ? InternalDialogues[2].y / 10f : null;
+        set => InternalDialogues[2] = new(InternalDialogues[2].x, value * 10f ?? 0f, InternalDialogues[2].z);
+    }
+
+    public int DialogueIdWhenInteractingWithShelvedItem
+    {
+        get => (int)InternalDialogues[6].y;
+        set => InternalDialogues[6] = new(InternalDialogues[6].x, value, InternalDialogues[6].z);
+    }
+
+    public float? ShelvedItemsInteractionRadius
+    {
+        get => InternalDialogues[8].x > 0.1f ? InternalDialogues[8].x / 10f : null;
+        set => InternalDialogues[8] = new(value * 10f ?? 0f, InternalDialogues[8].y, InternalDialogues[8].z);
+    }
+
+    public ReadOnlyCollection<ShelvedItemForSale> ItemsForSale { get; private set; } =
+        new List<ShelvedItemForSale>().AsReadOnly();
 
     public float MovementRadius
     {
@@ -45,14 +75,37 @@ public sealed class ItemsShopMapEntity : MapEntity
     internal override void InitializeFromNew()
     {
         InternalAnimIdOrItemId = 0;
+        InternalDialogues.AddRange(Enumerable.Repeat(Vector3.zero, 11));
     }
 
     internal override void InitializeFromExisting(IRegistryResolver registryResolver)
     {
         ILeavesRegistry<AnimIdLeaf> animIdsRegistry = registryResolver.Resolve<AnimIdLeaf>();
+        ILeavesRegistry<ItemLeaf> itemsRegistry = registryResolver.Resolve<ItemLeaf>();
 
         Behaviors.InitializeBehaviorFromExisting(registryResolver);
 
         AnimId = new(animIdsRegistry.LeavesByGameIds[InternalAnimIdOrItemId]);
+
+        List<ShelvedItemForSale> itemsForSale =
+            InternalData.Zip(
+                    InternalVectorData,
+                    (data, vectorData) =>
+                        new ShelvedItemForSale(new(itemsRegistry.LeavesByGameIds[data]), vectorData))
+                .ToList();
+        ChangeItemsForSale(itemsForSale);
+    }
+
+    public void ChangeItemsForSale(List<ShelvedItemForSale> itemsForSale)
+    {
+        InternalVectorData.Clear();
+        InternalData.Clear();
+        foreach (ShelvedItemForSale itemForSale in itemsForSale)
+        {
+            InternalData.Add(itemForSale.Item.GameId);
+            InternalVectorData.Add(itemForSale.Position);
+        }
+
+        ItemsForSale = itemsForSale.AsReadOnly();
     }
 }

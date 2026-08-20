da38f9c4cc462b13c0d816d8d985cedae8386702
aldelaro5 <aldelaro5@gmail.com>
2026-08-20 17:43:10 -0400

    Remove todo on enemy encounter

--- .VenusRootLoader/VenusRootLoader/Api/Leaves/MapEntities/Enemies/EnemyItemDrop.cs
+++ .VenusRootLoader/VenusRootLoader/Api/Leaves/MapEntities/Enemies/EnemyItemDrop.cs
@@ -3,7 +3,6 @@ using VenusRootLoader.LeavesInternals;
 
 namespace VenusRootLoader.Api.Leaves.MapEntities.Enemies;
 
-// TODO: Look into a way to fix the length 0 problem resulting in Vector3.zero which is a Crunchy Leaf drop
 public sealed class EnemyItemDrop
 {
     internal readonly Ref<Vector3> Vector3Ref = new(new(0, -1, 0));
